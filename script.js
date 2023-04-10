ceb165ddf78a9e2bf6dbbbbd1be451e5295ede01
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-04-10 14:45:37 +0530

    second

---
+++ script.js
@@ -0,0 +1,11 @@
+const headerTitle = document.getElementById('header-title');
+const header = document.getElementById('main-header');
+const items = document.querySelector('.title')
+
+headerTitle.textContent= 'Jay';
+headerTitle.style.color = 'black';
+
+header.style.border = 'white dotted 3px';
+
+items.style.fontWeight = 'bold'
+items.style.color = 'green';
