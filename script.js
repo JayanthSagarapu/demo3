3b1f21926344e82fa317b2b6449ea0e18ae6c68d
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-04-10 16:22:33 +0530

    updated script

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const headerTitle = document.getElementById('header-title');
 const header = document.getElementById('main-header');
-const items = document.querySelector('.title')
+const additems = document.querySelector('.title');
+const items = document.getElementsByClassName('list-group-item');
 
 headerTitle.textContent= 'Jay';
 headerTitle.style.color = 'black';
 
 header.style.border = 'white dotted 3px';
 
-items.style.fontWeight = 'bold'
-items.style.color = 'green';
+additems.style.fontWeight = 'bold'
+additems.style.color = 'green';
+
+items[2].style.background = 'green';
+
+for (let i=0; i< items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
