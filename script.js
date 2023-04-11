c05db01f4755a6c1ca066dfdd42a080b332e3efd
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-04-11 12:09:56 +0530

    updated script

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ headerTitle.style.color = 'black';
 header.style.border = 'white dotted 3px';
 
 additems.style.fontWeight = 'bold'
-additems.style.color = 'green';
+//additems.style.color = 'green';
 
 items[2].style.background = 'green';
 
@@ -21,3 +21,18 @@ for (let i=0; i< items.length; i++){
 for (let i=0; i< list.length; i++){
     list[i].style.background = 'grey';
 }
+
+const firstitem = document.querySelector('.list-group-item');
+const seconditem = document.querySelector('.list-group-item:nth-child(2)');
+const thirditem = document.querySelector('.list-group-item:nth-child(3)');
+const lastitem = document.querySelector('.list-group-item:last-child');
+
+//seconditem.style.background = 'green';
+thirditem.style.visibility = "hidden";
+
+const odd = document.querySelectorAll('li:nth-child(odd)');
+const even = document.querySelectorAll('li:nth-child(even)');
+for (let i=0; i< odd.length; i++){
+    odd[i].style.background = 'green';
+}
+even[0].style.color='green';
